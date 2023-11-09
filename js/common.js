79934f4c18901a1ecb63a32787ad7733bdb431d0
JesseYang0528 <yanghao7679@gmail.com>
2023-11-09 22:37:46 +1000

    try to make mobile nav scrollable

--- js/common.js
+++ js/common.js
@@ -333,7 +333,7 @@ function changeMobileMenu(id)
 function extentIframe()
 {
     const extentHeightPc = 315;
-    const extentHeightMobile = 500;
+    const extentHeightMobile = "100%";
     const iframe = document.getElementById('menu');
     let height = iframe.scrollHeight;
     // let width = window.screen.width;
